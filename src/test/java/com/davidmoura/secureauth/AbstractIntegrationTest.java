7e8a60af7e3f2ccafd19139bd9b3533243936508
DavidHMoura <davidhmoura@outlook.com>
2026-03-10 20:46:27 -0300

    test: migrate from H2 to Testcontainers with PostgreSQL and update README

---
+++ src/test/java/com/davidmoura/secureauth/AbstractIntegrationTest.java
@@ -0,0 +1,18 @@
+package com.davidmoura.secureauth;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine");
+}
